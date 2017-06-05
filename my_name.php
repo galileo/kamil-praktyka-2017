17f856984781a4868c3fb90b2988df54df721b99
gacko2k9 <gacko2k9@interia.pl>
2017-06-05 10:38:22 +0200

    Dodanie my_name.php

--- my_name.php
+++ my_name.php
@@ -1,10 +1,38 @@
 <?php
-$swiatla = 'zolte';
+error_reporting(E_ALL);
+ini_set('display_errors', true); // Errory w na stronie wskazują na daną linie i "opisują" problem
 
-if ($swiatla === 'zielone') {
-    echo 'Zielone swiatlo, proszę jechać';
-} else if ($swiatla === 'zolte') {
-    echo 'Zolte swiatlo, proszę zwolnic';
-} else {
-    echo 'Czerwone swiatlo, prosze się zatrzymać';
+class Swiatlo 
+{
+    public $swiatla = 'zielone'; // Zadeklraowanie zmiennej $swiatla
+    
+    function __construct($swiatla = 'zielone') //Zadaniem konstruktora jest jest wykonanie pewnych akcji tuż po utworzeniu obiektu tak, aby można było od razu zacząć z nim pracę. 
+    {
+        $this->swiatla = $swiatla;
+    }
+    
+    public function info() // Funkcja info
+    {
+        $swiatla = $this->swiatla;
+        if ('zielone' === $swiatla) {
+            echo 'Zielone swiatlo, proszę jechać'; // Dla wartości zielone, wypisze sie "Zielone swiatlo, proszę jechać"
+        } else if ('zolte' === $swiatla) {
+            echo 'Zolte swiatlo, proszę zwolnic'; // Dla wartości zolte, wypisze sie "Zolte swiatlo, proszę zwolnic"
+        } else if ('czerwone' === $swiatla) {
+            echo 'Czerwone swiatlo, prosze się zatrzymać'; // Dla wartości czerwone, wypisze sie "Czerwone swiatlo, prosze się zatrzymać"
+        } else {
+            echo 'nie istnieje'; // Dla kazdego innego wpisanego tekstu wyświetli się "nie istnieje"
+        }
+        
+    }
+    
+    public function tylkoCzerwone() // Druga funkcja "Tylko czerwone"
+    {
+        if ($this->swiatla === 'czerwone') { 
+            echo "Czerwony jak cegla"; // Jeżeli przyjmie wartość czerwone to wypisze się "Czerwony jak cegła"
+        }
+    }
 }
+
+$swiatlo = new swiatlo('zielone'); // Przypisanie wartosci zmiennej $swiatlo
+$swiatlo->info(); // Wywołanie funkcji info
