f2244b73f4c1a494e00122f2235c031ba84b4eca
gacko2k9 <gacko2k9@interia.pl>
2017-06-12 12:36:12 +0200

    Poprawa AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -39,13 +39,6 @@
     <div class="prawa">
     <h1><?php echo $page->getTitle() ?></h1>
     <p><?php echo $page->getContent() ?></p>
-    <div class="galeria">
-      <img src="img\chaber.jpg" alt="Róża" height="500" width="500" />
-      <div class="opisykwiatow">
-        gatunek rośliny zielnej z rodziny astrowatych. Nazwy ludowe: białasy, głowacz, jasieniec, kardy, kwiatek wołoszek, macoszka,
-        modrak, modrzeńczyk, samosiejka, wasilek, wawer. Występuje w Europie oraz na Syberii Zachodniej. W Polsce pospolity chwast<
-        polny, archeofitczasem podaje się nawet dwukrotnie większą liczbę, co wynika z różnego traktowania taksonów. Większość 
-        ozdobnych odmian
       </div>
       <div class="clear" />
       </div>
